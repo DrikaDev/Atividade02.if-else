504d53305c3463cb9a3f99e8716c7326fab7ce36
Adriana Gutierrez <102387476+DrikaDev@users.noreply.github.com>
2022-06-25 20:37:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
-let idade = 18
-let condicaoHumana = true //Luan, o que seria isso? Condição humana? É pra afirmar se é humano ou animal? rs
-let janeiro = true
-let dezembro = true
 const nome = "Adriana"
-let primeiraLetraDoNome = "A" 
-const sobrenome = "Gutierrez"
-let qtdLetrasSobrenome = 9
+console.log (nome.length) //7 letras
 
 //Crie uma condição que exiba uma mensagem no console "se sua idade for maior que 18".
-if (idade >= 18)
-    console.log (nome + " é maior de idade!")
+let idade = 42
+if (idade >= 18){
+    console.log ("Você é maior de idade!")
+}else{
+    console.log("Você é menor de idade")
+}
 
 //Crie uma condição que exiba uma mensagem no console "se você for maior de idade" E "se a condição humana for true".
-if ((idade >= 18) && (condicaoHumana === true)) 
-    console.log (nome + " é uma humana maior de idade!")
+let condicaoHumana = false
+if ((idade >= 18) && (condicaoHumana === true)){
+    console.log ("Você é um humano maior de idade!")
+}else{
+    console.log ("Você é um robô!")
+}
 
 //Criar uma condição que exiba uma mensagem no console "se você fizer aniversário em Janeiro OU Dezembro".
-if ((janeiro === true) || (dezembro === true))
+let janeiro = true
+let dezembro = true
+let mes = "julho"
+if ((mes === janeiro) || (mes === dezembro)){
     console.log ("Feliz Aniversário!")
+}else{
+    console.log("Você não vai ganhar presente!")
+}
+    
 
 //Crie uma condição que exiba uma mensagem no console "se seu nome começar com a letra R".
-if (primeiraLetraDoNome === "R") //Luan, meu nome começa com A, então eu entendi que NÃO é pra aparecer no console, certo?
-    console.log (nome + ", seu nome começa a letra A.")
-
+if (nome[0] === "R"){
+    console.log ("Seu nome começa com a letra R");
+}else{
+    console.log(`Seu nome começa com a letra ${nome[0]}`);
+    // ou console.log("Seu nome começa com " + nome[0]);
+    // console.log(`Seu nome termina com a letra ${nome[nome.length - 1]}`);
+}
+    
 //Crie uma condição que exiba uma mensagem no console "se seu sobrenome tiver mais de 6 letras" 
 //OU "se seu nome começar com a letra E".
-if ((qtdLetrasSobrenome >= 6) || (primeiraLetraDoNome === E)) // Luan, como um deles é True, então ele aparece no console, certo?
-    console.log(nome + ", seu sobrenome tem mais de 6 letras, porém seu nome começa com a letra A")
+const sobrenome = "Gutierrez"
+if ((sobrenome.length > 6) || (nome[0] === E)){
+    console.log(sobrenome + ", seu sobrenome tem mais de 6 letras");
+}else{
+    console.log(nome + "Seu nome começa com a letra A")
+}
